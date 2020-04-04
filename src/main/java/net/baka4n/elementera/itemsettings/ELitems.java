5d1b846f573a6ef95f5f9f1a7bf6823b8ef0a2d5
stepheninchina <60695447+stepheninchina@users.noreply.github.com>
2020-04-04 13:20:34 +0800

    edit helium

--- src/main/java/net/baka4n/elementera/itemsettings/ELitems.java
+++ src/main/java/net/baka4n/elementera/itemsettings/ELitems.java
@@ -8,9 +8,9 @@ public class ELitems implements ItemReg {
     public static final Protium protium = new Protium();
     public static final Deuterium deuterium = new Deuterium();
     public static final Tritium tritium = new Tritium();
-    public static final Helium helium = new Helium();
+    public static final Helium helium_2 = new Helium();
     public static final Helium helium_3 = new Helium();
-    public static final Helium helium_4 = new Helium();
+    public static final Helium helium = new Helium();
     public static final Helium helium_5 = new Helium();
     public static final Helium helium_6 = new Helium();
     public static final Helium helium_7 = new Helium();
@@ -25,9 +25,9 @@ public class ELitems implements ItemReg {
         registerItem("protium", protium);
         registerItem("deuterium", deuterium);
         registerItem("tritium", tritium);
-        registerItem("helium", helium);
+        registerItem("helium_2", helium_2);
         registerItem("helium_3", helium_3);
-        registerItem("helium_4", helium_4);
+        registerItem("helium_4", helium);
         registerItem("helium_5", helium_5);
         registerItem("helium_6", helium_6);
         registerItem("helium_7", helium_7);
