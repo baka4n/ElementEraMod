342d8c39931f6d7000da342a4279022135eec301
stepheninchina <60695447+stepheninchina@users.noreply.github.com>
2020-04-24 17:05:59 +0800

    Update ModItemSetting.java

--- src/main/java/io/github/elementera/ModItemSetting.java
+++ src/main/java/io/github/elementera/ModItemSetting.java
@@ -14,389 +14,136 @@ public class ModItemSetting extends Item {
     }
 }
 class Protium extends Item {
-    public Protium() {
-        super(new Item.Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public Protium() { super(new Item.Settings().group(GAS).maxCount(16)); }
 }class Deuterium extends Item {
-    public Deuterium() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public Deuterium() { super(new Settings().group(GAS).maxCount(16)); }
 }class Tritium extends Item {
-    public Tritium() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public Tritium() { super(new Settings().group(GAS).maxCount(16)); }
 }
 class He2 extends Item {
-    public He2() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public He2() { super(new Settings().group(GAS).maxCount(16)); }
 }class He3 extends Item {
-    public He3() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public He3() { super(new Settings().group(GAS).maxCount(16)); }
 }class He4 extends Item {
-    public He4() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public He4() { super(new Settings().group(GAS).maxCount(16)); }
 }class He5 extends Item {
-    public He5() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-
-    }
+    public He5() { super(new Settings().group(GAS).maxCount(16)); }
 }class He6 extends Item {
-    public He6() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public He6() { super(new Settings().group(GAS).maxCount(16)); }
 }class He7 extends Item {
-    public He7() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public He7() { super(new Settings().group(GAS).maxCount(16)); }
 }class He8 extends Item {
-    public He8() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public He8() { super(new Settings().group(GAS).maxCount(16)); }
 }class He9 extends Item {
-    public He9() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public He9() { super(new Settings().group(GAS).maxCount(16)); }
 }class He10 extends Item {
-    public He10() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public He10() { super(new Settings().group(GAS).maxCount(16)); }
 
 }
 class Li6 extends Item {
-    public Li6() {
-        super(new Settings()
-                .group(METALS)
-                .maxCount(16));
-    }
+    public Li6() { super(new Settings().group(METALS).maxCount(16)); }
 }class Li7 extends Item {
-    public Li7() {
-        super(new Settings()
-                .group(METALS)
-                .maxCount(16));
-    }
+    public Li7() { super(new Settings().group(METALS).maxCount(16)); }
 }
 class Be7 extends Item {
-    public Be7() {
-        super(new Settings()
-                .group(METALS)
-                .maxCount(16));
-    }
+    public Be7() { super(new Settings().group(METALS).maxCount(16)); }
 }class Be8 extends Item {
-    public Be8() {
-        super(new Settings()
-                .group(METALS)
-                .maxCount(16));
-    }
+    public Be8() { super(new Settings().group(METALS).maxCount(16)); }
 }class Be9 extends Item {
-    public Be9() {
-        super(new Settings()
-                .group(METALS)
-                .maxCount(16));
-    }
+    public Be9() { super(new Settings().group(METALS).maxCount(16)); }
 }class Be10 extends Item {
-    public Be10() {
-        super(new Settings()
-                .group(METALS)
-                .maxCount(16));
-    }
+    public Be10() { super(new Settings().group(METALS).maxCount(16)); }
 }
 class B6 extends Item {
-    public B6() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B6() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B7 extends Item {
-    public B7() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B7() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B8 extends Item {
-    public B8() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B8() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B9 extends Item {
-    public B9() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B9() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B10 extends Item {
-    public B10() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B10() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B11 extends Item {
-    public B11() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B11() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B12 extends Item {
-    public B12() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B12() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B13 extends Item {
-    public B13() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B13() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B14 extends Item {
-    public B14() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B14() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B15 extends Item {
-    public B15() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B15() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B16 extends Item {
-    public B16() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B16() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B17 extends Item {
-    public B17() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B17() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B18 extends Item {
-    public B18() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B18() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class B19 extends Item {
-    public B19() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public B19() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }
 class C8 extends Item {
-    public C8() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C8() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C9 extends Item {
-    public C9() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C9() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C10 extends Item {
-    public C10() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C10() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C11 extends Item {
-    public C11() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C11() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C12 extends Item {
-    public C12() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C12() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C13 extends Item {
-    public C13() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C13() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C14 extends Item {
-    public C14() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C14() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C15 extends Item {
-    public C15() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C15() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C16 extends Item {
-    public C16() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C16() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C17 extends Item {
-    public C17() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C17() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C18 extends Item {
-    public C18() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C18() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C19 extends Item {
-    public C19() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C19() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C20 extends Item {
-    public C20() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C20() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C21 extends Item {
-    public C21() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C21() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }class C22 extends Item {
-    public C22() {
-        super(new Settings()
-                .group(NONMETALS)
-                .maxCount(16));
-    }
+    public C22() { super(new Settings().group(NONMETALS).maxCount(16)); }
 }
 class N10 extends Item {
-    public N10() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N10() { super(new Settings().group(GAS).maxCount(16)); }
 }class N11 extends Item {
-    public N11() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N11() { super(new Settings().group(GAS).maxCount(16)); }
 }class N12 extends Item {
-    public N12() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N12() { super(new Settings().group(GAS).maxCount(16)); }
 }class N13 extends Item {
-    public N13() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N13() { super(new Settings().group(GAS).maxCount(16)); }
 }class N14 extends Item {
-    public N14() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N14() { super(new Settings().group(GAS).maxCount(16)); }
 }class N15 extends Item {
-    public N15() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N15() { super(new Settings().group(GAS).maxCount(16)); }
 }class N16 extends Item {
-    public N16() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N16() { super(new Settings().group(GAS).maxCount(16)); }
 }class N17 extends Item {
-    public N17() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N17() { super(new Settings().group(GAS).maxCount(16)); }
 }class N18 extends Item {
-    public N18() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N18() { super(new Settings().group(GAS).maxCount(16)); }
 }class N19 extends Item {
-    public N19() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N19() { super(new Settings().group(GAS).maxCount(16)); }
 }class N20 extends Item {
-    public N20() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N20() { super(new Settings().group(GAS).maxCount(16)); }
 }class N21 extends Item {
-    public N21() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N21() { super(new Settings().group(GAS).maxCount(16)); }
 }class N22 extends Item {
-    public N22() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N22() { super(new Settings().group(GAS).maxCount(16)); }
 }class N23 extends Item {
-    public N23() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N23() { super(new Settings().group(GAS).maxCount(16)); }
 }class N24 extends Item {
-    public N24() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N24() { super(new Settings().group(GAS).maxCount(16)); }
 }class N25 extends Item {
-    public N25() {
-        super(new Settings()
-                .group(GAS)
-                .maxCount(16));
-    }
+    public N25() { super(new Settings().group(GAS).maxCount(16)); }
 }
